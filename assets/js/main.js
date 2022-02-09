c5de599de2dc0e64293e25f3269835ac77d57af6
Jake Bayford <jake.bayford@reveriez.co.uk>
2022-02-09 16:25:27 +0000

    Version 1.0.0-20220209
    Initial commit dev version

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+(function() {
+	// Set current year
+	const footerYear = document.getElementById('year');
+	footerYear.innerText = new Date().getFullYear();
+	
+	// Check for formsubmit.co redirect
+	const urlParams = new URLSearchParams(window.location.search);
+	if(urlParams.get('sent') === 'true') {
+		const dom = document.getElementById('dom');
+		dom.classList.remove('smooth');
+		const modal = document.getElementById('sentmodal');
+		modal.classList.add('show');
+		setTimeout(function(){ 
+			modal.classList.remove('show'); 
+			dom.classList.add('smooth'); 
+		}, 5000);
+	}
+	
+	// Reset zoom level on mobile devices
+	const isMobileDevice = window.matchMedia("only screen and (max-width: 500px)").matches;
+	const browserZoomLevel = Math.round(window.devicePixelRatio * 100);
+	if(isMobileDevice && browserZoomLevel > 100) {
+		document.body.style.zoom = (window.innerWidth / window.outerWidth);
+	}
+
+	// Contact form handler
+	document.getElementById('contact-form').addEventListener("submit", function(e){
+		if (!grecaptcha.getResponse()) {
+			event.preventDefault();
+			grecaptcha.execute();
+		}
+	});
+})();
+
+// Google recaptcha callback handler
+onSubmit = function() {
+	document.getElementById('contact-form').submit();
+}
